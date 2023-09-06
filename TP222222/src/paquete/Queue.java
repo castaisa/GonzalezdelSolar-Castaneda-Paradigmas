5cde4ce99cfdd3ff9256893c97d56287a158bdce
isa <icastaneda@udes.edu.ar>
2023-09-06 00:00:50 -0300

    a ver si funciona

---
+++ TP222222/src/paquete/Queue.java
@@ -0,0 +1,30 @@
+package paquete;
+
+public class Queue {
+
+  public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	public Queue add( Object  cargo ) {
+		// TODO Auto-generated method stub
+		return this;
+	}
+
+	public Object take() {
+    // TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object head() {
+		// TODO Auto-generated method stub
+    return null;
+	}
+
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
